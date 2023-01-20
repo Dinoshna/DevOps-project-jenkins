588e9f40a6dd9d3b225647cddb66a4192ff4ae9b
Dinoshna Thathuru <dinulearnings@gmail.com>
2023-01-20 23:12:21 +0530

    code

---
+++ src/main/java/com/deploy/jenkinspipelinedockerProj/JenkinsPipelineDockerProjApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.jenkinspipelinedockerProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineDockerProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineDockerProjApplication.class, args);
+	}
+
+}
